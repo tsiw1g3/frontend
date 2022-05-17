748225d43b8c1fa7f792e5efba49121c6d74ee8b
Gabriel Goulart <gabriel_goulart@hotmail.com>
2022-05-17 06:50:20 -0300

    Adds Viewbanca and Users routes

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import Register from "./Pages/Register";
 import Dashboard from "./Pages/Dashboard";
 import Addition from "./Pages/Addition";
 import ViewBoard from "./Pages/ViewBoard";
+import Users from "./Pages/Users";
+import ViewBanca from "./Pages/ViewBanca";
 
 const Routes = () => {
   return (
@@ -17,8 +19,10 @@ const Routes = () => {
       <Route component={Login} path="/login" exact />
       <Route component={Register} path="/register" exact />
       <Route component={Dashboard} path="/dashboard" exact />
+      <Route component={ViewBanca} path="/verbanca" exact />
       <Route component={Addition} path="/addition" exact />
-      <Route component={ViewBoard} path="/verbanca" exact />
+      <Route component={ViewBoard} path="/editarbanca" exact />
+      <Route component={Users} path="/users" exact />
     </>
   );
 };
