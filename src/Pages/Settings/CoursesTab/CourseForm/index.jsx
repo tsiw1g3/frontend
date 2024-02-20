cafe51a58991cd72fac9f1a7b253ccc2671ec5e3
João Pedro Brito Silva <contato.joaocomp@gmail.com>
2024-02-20 18:17:50 -0300

    feat: implements modal to edit courses

---
+++ src/Pages/Settings/CoursesTab/CourseForm/index.jsx
@@ -0,0 +1,130 @@
+import {
+  Button,
+  Grid,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@material-ui/core";
+import { Field, Form } from "react-final-form";
+import React from "react";
+
+import { TextField } from "final-form-material-ui";
+
+const themeEditar = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#329F5B",
+      dark: "#184e2d",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default function CourseForm({ course, onSubmit }) {
+  const validate = (values) => {
+    const errors = {};
+    const requiredFields = [
+      "nome",
+      "sigla",
+      "disciplina",
+      "coordenacao",
+      "cargo_coordenacao",
+    ];
+
+    requiredFields.forEach((field) => {
+      if (!values[field]) errors[field] = "Obrigatório";
+    });
+
+    return errors;
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", padding: "32px" }}>
+      <Typography variant="h6" gutterBottom>
+        {course ? `Editar Curso - ${course.nome}` : "Adicionar Curso"}
+      </Typography>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{
+          id: "",
+          nome: "",
+          sigla: "",
+          disciplina: "",
+          coordenacao: "",
+          cargo_coordenacao: "",
+          ...course,
+        }}
+        validate={validate}
+        render={({ submitting, values, handleSubmit }) => (
+          <form onSubmit={handleSubmit} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={9}>
+                <Field
+                  component={TextField}
+                  label="Nome do Curso"
+                  name="nome"
+                  type="text"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Field
+                  component={TextField}
+                  label="Sigla"
+                  name="sigla"
+                  type="text"
+                  fullWidth
+                  value={values.sigla}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  component={TextField}
+                  label="Disciplina Final"
+                  name="disciplina"
+                  type="text"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  component={TextField}
+                  label="Coordenador(a)"
+                  name="coordenacao"
+                  type="text"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  component={TextField}
+                  label="Título do(a) coordenador(a)"
+                  name="cargo_coordenacao"
+                  type="text"
+                  fullWidth
+                  multiple
+                />
+              </Grid>
+              <Grid item xs={9} />
+              <Grid item xs={3}>
+                <ThemeProvider theme={themeEditar}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    style={{ borderRadius: 10 }}
+                    disabled={submitting}
+                    fullWidth
+                  >
+                    Salvar Curso
+                  </Button>
+                </ThemeProvider>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      />
+    </div>
+  );
+}
--- src/Pages/Settings/CoursesTab/index.jsx
+++ src/Pages/Settings/CoursesTab/index.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import { makeStyles } from "@material-ui/styles";
 import { DataGrid } from "@mui/x-data-grid";
 import ReactLoading from "react-loading";
 
-import { TextField } from "@material-ui/core";
+import { Box, Modal, TextField } from "@material-ui/core";
 import useCourses from "Hooks/Users/useCourses";
+import CourseForm from "./CourseForm";
 
 const useStyles = makeStyles({
   root: {
@@ -20,39 +21,125 @@ const useStyles = makeStyles({
   },
 });
 
+const COURSES_MODAL_ACTIONS = {
+  OPEN_MODAL: "OPEN_MODAL",
+  CLOSE_MODAL: "CLOSE_MODAL",
+  UPDATE_SELECTED_COURSE: "UPDATE_SELECTED_COURSE",
+};
+
+const INITIAL_STATE = {
+  open: false,
+  selectedCourse: undefined,
+};
+
+function useCoursesModalReducer(state = INITIAL_STATE, action = null) {
+  switch (action.type) {
+    case COURSES_MODAL_ACTIONS.OPEN_MODAL:
+      return { ...state, open: true };
+    case COURSES_MODAL_ACTIONS.CLOSE_MODAL:
+      return { ...state, open: false };
+    case COURSES_MODAL_ACTIONS.UPDATE_SELECTED_COURSE:
+      return { ...state, selectedCourse: action.payload };
+    default:
+      return state;
+  }
+}
+
+function useCoursesModal() {
+  const [state, dispatch] = useReducer(useCoursesModalReducer, INITIAL_STATE);
+
+  const openModal = () => dispatch({ type: COURSES_MODAL_ACTIONS.OPEN_MODAL });
+  const closeModal = () =>
+    dispatch({ type: COURSES_MODAL_ACTIONS.CLOSE_MODAL });
+
+  const updateSelectedCourse = (course) =>
+    dispatch({
+      type: COURSES_MODAL_ACTIONS.UPDATE_SELECTED_COURSE,
+      payload: course,
+    });
+
+  return { ...state, updateSelectedCourse, openModal, closeModal };
+}
+
 export default function CoursesTab() {
   const classes = useStyles();
   const { courses, loading, handleSearch, handleEdit } = useCourses();
+  const { open, updateSelectedCourse, selectedCourse, openModal, closeModal } =
+    useCoursesModal();
 
   const columns = [
-    { field: "sigla", headerName: "Sigla", minWidth: 150, editable: true },
+    {
+      field: "sigla",
+      headerName: "Sigla",
+      minWidth: 150,
+      headerAlign: "center",
+      align: "center",
+    },
     {
       field: "nome",
       headerName: "Nome",
       minWidth: 300,
       flex: 1,
-      editable: true,
+      headerAlign: "center",
+      align: "center",
     },
     {
       field: "disciplina",
       headerName: "Disciplina de Conclusão",
-      editable: true,
       minWidth: 250,
+      headerAlign: "center",
+      align: "center",
     },
     {
       field: "coordenacao",
       headerName: "Coordenador",
-      editable: true,
       minWidth: 300,
+      headerAlign: "center",
+      align: "center",
     },
     {
       field: "cargo_coordenacao",
       headerName: "Título do Coordenador",
-      editable: true,
       minWidth: 300,
       flex: 1,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "id",
+      headerName: "Ações",
+      renderCell: CoursesAction,
+      sortable: false,
+      headerAlign: "center",
+      align: "center",
     },
   ];
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 800,
+    bgcolor: "background.paper",
+    borderRadius: 4,
+    padding: "4px",
+  };
+
+  function CoursesAction({ row }) {
+    return (
+      <button
+        title="Editar banca"
+        name="edit-board"
+        type="submit"
+        id="edit-board"
+        onClick={() => {
+          updateSelectedCourse(row);
+          openModal();
+        }}
+      />
+    );
+  }
 
   return (
     <>
@@ -73,6 +160,17 @@ export default function CoursesTab() {
         </div>
       ) : (
         <>
+          <Modal open={open} onClose={closeModal}>
+            <Box sx={style}>
+              <CourseForm
+                course={selectedCourse}
+                onSubmit={(values) => {
+                  handleEdit(values);
+                  closeModal();
+                }}
+              />
+            </Box>
+          </Modal>
           <TextField
             id="banca-search"
             label="Buscar Usuários"
@@ -85,11 +183,10 @@ export default function CoursesTab() {
             columns={columns}
             pageSize={10}
             className={classes.root}
-            autoHeight={true}
-            disableColumnMenu={true}
-            disableColumnFilter={true}
-            editMode="row"
-            onRowEditStop={({ row: course }) => handleEdit(course)}
+            autoHeight
+            disableColumnMenu
+            disableColumnFilter
+            disableSelectionOnClick
           />
         </>
       )}
